1d09b601852f85c88319c48349d0c775360195e9
tolemariamfufat <tolemariamfufat@gmail.com>
2024-08-24 22:25:31 +0300

    user_registration is added

--- formtutorial/members/views.py
+++ formtutorial/members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -24,6 +25,27 @@ def logout_user(request):
 
     messages.success(request, ("There are logged out!"))
     return redirect('index')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration is successful!"))
+            return redirect('index')
+    else:
+        form = UserCreationForm()                                  
+
+
+
+    return render(request, 'members/register_user.html', {
+        'form': form,
+    })
+
             
 
 
